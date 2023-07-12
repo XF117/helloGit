52e9767482681dd636b3e5f7dcc627c293e65d37
XF117 <1173120225@qq.com>
2023-07-12 22:02:02 +0800

    01更新

--- HelloGit/src/main/java/cn/xiaofeng/gitDemo.java
+++ HelloGit/src/main/java/cn/xiaofeng/gitDemo.java
@@ -3,5 +3,6 @@ package cn.xiaofeng;
 public class gitDemo {
     public static void main(String[] args) {
         System.out.println("Hello Git!Test!");
+        System.out.println("Hello Git!Test!0");
     }
 }
