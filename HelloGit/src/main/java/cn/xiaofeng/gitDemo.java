b839e8bcba95d4e43c8cf2da10c9673beef31c8e
XF117 <1173120225@qq.com>
2023-07-12 22:10:59 +0800

    1创建分支

--- HelloGit/src/main/java/cn/xiaofeng/gitDemo.java
+++ HelloGit/src/main/java/cn/xiaofeng/gitDemo.java
@@ -4,5 +4,6 @@ public class gitDemo {
     public static void main(String[] args) {
         System.out.println("Hello Git!Test!");
         System.out.println("Hello Git!Test!0");
+        System.out.println("Hello Git!Test!1创建分支");
     }
 }
